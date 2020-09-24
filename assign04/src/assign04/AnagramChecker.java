d87069f02757231fa80442741f2a648f50e7e97b
jonpokemon43@outlook.com <meepmeep>
2020-09-24 15:54:33 -0600

    debugging.

--- assign04/src/assign04/AnagramChecker.java
+++ assign04/src/assign04/AnagramChecker.java
@@ -9,17 +9,19 @@ import java.util.Scanner;
 
 /**
  * 
- * @author Dixit  & Krummenacher
+ * @author Dixit & Krummenacher
  *
  */
-public class AnagramChecker {
-	
+public class AnagramChecker
+{
+
 	/**
 	 * Using insertion sort , this method sorts an String lexicographically.
+	 * 
 	 * @param inputString
 	 * @return resultString that's sorted
 	 */
-	public static String sort(String inputString) 
+	public static String sort(String inputString)
 	{
 		// Checks for empty Strings
 		if (inputString.equals(null))
@@ -29,77 +31,84 @@ public class AnagramChecker {
 		char targetVal;
 		char[] charArray = inputString.toCharArray();
 		int loopCounter;
-		for (int index = 1; index < inputString.length(); index++) // goes through every of the unsorted array except															// index 0(already sorted)
+		for (int index = 1; index < inputString.length(); index++) // goes through every of the unsorted array except //
+																	// index 0(already sorted)
 		{
 			targetVal = charArray[index];
-			for (loopCounter = index - 1; loopCounter >= 0 && charArray[loopCounter] > targetVal; loopCounter--) {
+			for (loopCounter = index - 1; loopCounter >= 0 && charArray[loopCounter] > targetVal; loopCounter--)
+			{
 				charArray[loopCounter + 1] = charArray[loopCounter];
 			}
 			charArray[loopCounter + 1] = targetVal;
 		}
 		return new String(charArray);
 	}
-	
+
 	/**
-	 * Sorts an generic array with insertion sort and uses Comparator object for comparison
+	 * Sorts an generic array with insertion sort and uses Comparator object for
+	 * comparison
+	 * 
 	 * @param <T>
 	 * @param inputTArray
 	 * @param inputComp
 	 */
-	public static <T> void insertionSort(T[] inputArr, Comparator<? super T> inputComp) 
+	public static <T> void insertionSort(T[] inputArr, Comparator<? super T> inputComp)
 	{
-	T key;
-	int loopIndex;
-	for(int index=1; index<inputArr.length; index++)
-	{
-		key= inputArr[index];
-		
-		for(loopIndex= index-1; loopIndex>=0 && inputComp.compare(inputArr[loopIndex], key)> 0 ;loopIndex--)
+		T key;
+		int loopIndex;
+		for (int index = 1; index < inputArr.length; index++)
 		{
-			inputArr[loopIndex+1]=inputArr[loopIndex];
+			key = inputArr[index];
+
+			for (loopIndex = index - 1; loopIndex >= 0 && inputComp.compare(inputArr[loopIndex], key) > 0; loopIndex--)
+			{
+				inputArr[loopIndex + 1] = inputArr[loopIndex];
+			}
+			inputArr[loopIndex + 1] = key;
 		}
-		inputArr[loopIndex+1]=key;
-	}	
 	}
-	
+
 	/**
-	 * Checks whether the two given strings are anagrams or not.Returns true if they are.
+	 * Checks whether the two given strings are anagrams or not.Returns true if they
+	 * are.
+	 * 
 	 * @param inputString
 	 * @param secondInputString
 	 * @return
 	 */
-	public static boolean areAnagrams(String inputString, String secondInputString) 
+	public static boolean areAnagrams(String inputString, String secondInputString)
 	{
 		return sort(inputString).equals(sort(secondInputString));
 	}
-	
+
 	/**
-	 * This method finds the largest group of anagrams in a Array of Strings. Returns an empty array if no anagrams are present.
+	 * This method finds the largest group of anagrams in a Array of Strings.
+	 * Returns an empty array if no anagrams are present.
+	 * 
 	 * @param inputStringArray
 	 * @return
 	 */
-	public static String[] getLargestAnagramGroup(String[] inputStringArray) 
+	public static String[] getLargestAnagramGroup(String[] inputStringArray)
 	{
 		ArrayList<String> resultList = new ArrayList<String>();
-		String[] result=null;
+		String[] result = null;
 		int max = 0;
 		int currentMax = 1;
-		
-		insertionSort(inputStringArray,(a, b) ->sort(a).compareTo(sort(b)));
-		
-		if(inputStringArray.length > 0)
+
+		insertionSort(inputStringArray, (a, b) -> sort(a).compareTo(sort(b)));
+
+		if (inputStringArray.length > 0)
 		{
-			for(int index = 0; index<inputStringArray.length-1; index++)
+			for (int index = 0; index < inputStringArray.length - 1; index++)
 			{
-				if(areAnagrams(inputStringArray[index], inputStringArray[index+1]))
+				if (areAnagrams(inputStringArray[index], inputStringArray[index + 1]))
 				{
 					currentMax++;
 					resultList.add(inputStringArray[index]);
-					result = (String[]) resultList.toArray(new String[resultList.size()]);	
-				}
-				else
+					result = (String[]) resultList.toArray(new String[resultList.size()]);
+				} else
 				{
-					if(currentMax > max)
+					if (currentMax > max)
 					{
 						resultList.add(inputStringArray[index]);
 						max = currentMax;
@@ -110,16 +119,18 @@ public class AnagramChecker {
 				}
 			}
 		}
-		if(result.length == 1)
+		if (result.length == 1)
 			result = new String[0];
 		return result;
 	}
-	
+
 	/**
-	 * This method finds the largest group of anagrams in a File. Returns an empty array if no anagrams are present.
+	 * This method finds the largest group of anagrams in a File. Returns an empty
+	 * array if no anagrams are present.
+	 * 
 	 * @param filename
 	 * @return
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public static String[] getLargestAnagramGroup(String filename)
 	{
@@ -130,36 +141,35 @@ public class AnagramChecker {
 		{
 			file = new File(filename);
 			scn = new Scanner(file);
-		}
-		catch(FileNotFoundException e)
+		} catch (FileNotFoundException e)
 		{
 			return new String[0];
 		}
-		
-		while(scn.hasNext())
+
+		while (scn.hasNext())
 		{
 			result.add(scn.next());
 		}
 		scn.close();
-		
+
 		return getLargestAnagramGroup(result.toArray(new String[result.size()]));
 	}
-	
-	public static  <T> void main(String[] args) 
+
+	public static <T> void main(String[] args)
 	{
-			System.out.println(sort("abc"));
-			System.out.println(sort("cba"));
-			System.out.println(sort("cbaABDA"));
-			System.out.println(sort("yourS"));
-			System.out.println(areAnagrams("RonaldReagan","Adarnlongera"));
-			String [] arr = new String[4];
-			arr[0]="cba";
-			//arr[1]="abc";
-			arr[1]="cbaABDA";
-			arr[2]="yourS";
-			arr[3]="anagrams";
-			//arr[5]="bac";
-			System.out.println(Arrays.toString(getLargestAnagramGroup(arr)));
-			
+		System.out.println(sort("abc"));
+		System.out.println(sort("cba"));
+		System.out.println(sort("cbaABDA"));
+		System.out.println(sort("yourS"));
+		System.out.println(areAnagrams("RonaldReagan", "Adarnlongera"));
+		String[] arr = new String[4];
+		arr[0] = "cba";
+		// arr[1]="abc";
+		arr[1] = "cbaABDA";
+		arr[2] = "yourS";
+		arr[3] = "anagrams";
+		// arr[5]="bac";
+		System.out.println(Arrays.toString(getLargestAnagramGroup(arr)));
+
 	}
 }
--- assign04/src/assign04/AnagramCheckerTester.java
+++ assign04/src/assign04/AnagramCheckerTester.java
@@ -2,42 +2,162 @@ package assign04;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AnagramCheckerTester
 {
-	private String s1,s2;
-	private String[] smallList , emptyList ,midList;
+	private String s1, s2;
+	private String[] smallList, emptyList, midList;
 	private AnagramChecker d;
+	private Comparator comp;
 
 	@BeforeEach
 	public void setUp()
 	{
-		 smallList = new String[5];
-		 emptyList = new String[0];
-	      midList = new String[10];
+		smallList = new String[5];
+		emptyList = new String[0];
+		midList = new String[10];
+
+		smallList[0] = "cat";
+		smallList[1] = "the";
+		smallList[2] = "act";
+		smallList[3] = "teh";
+		smallList[4] = "meep";
 		
-		smallList[0] = "";
-		smallList[1] = "";
-		smallList[2] = "";
-		smallList[3] = "";
-		smallList[4] = "";
-		
-		
-		s1="racecar";
+		midList[0] = "CAT";
+		midList[1] = "cat";
+		midList[2] = "meep";
+		midList[3] = "your";
+		midList[4] = "dcad";
+		midList[5] = "mepE";
+		midList[6] = "ACDC";
+		midList[7] = "moop";
+		midList[8] = "act";
+		midList[9] = "lsat";
+
+		s1 = "racecar";
+		comp = (a, b) -> AnagramChecker.sort((String) a).compareTo(d.sort((String) b));
 	}
+
+	/**
+	 * this sets the use of sort an a small String
+	 */
 	@Test
 	void sort_Small()
 	{
 		String result = d.sort(s1);
-		assertEquals(result,"aaccerr");
-		
+		assertEquals(result, "aaccerr");
+
 	}
+
+	/**
+	 * this sets the use of sort on an empty String
+	 */
+	@Test
+	void sort_EmptyString()
+	{
+		String result = d.sort("");
+		assertEquals(result, "");
+	}
+
+	/**
+	 * this sets the use of sort on an null value.
+	 */
+	@Test
+	void sort_NullInput()
+	{
+		assertThrows(NullPointerException.class, () -> {
+			d.sort(null);
+		});
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void insertionSort_smallList()
+	{
+		d.insertionSort(smallList, comp);
+		String[] key = new String[5];
+		
+		key[0] = "act";
+		key[1] = "cat";
+		key[2] = "meep";
+		key[3] = "teh";
+		key[4] = "the";		
+		
+		assertEquals(smallList.toString(), key.toString());
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void insertionSort_midList()
+	{
+		d.insertionSort(midList, comp);
+		String[] key = new String[5];
+		
+		key[0] = "act";
+		key[1] = "cat";
+		key[2] = "dcad";
+		key[3] = "meep";
+		key[4] = "mepE";
+		key[5] = "moop";
+		key[6] = "lsat";
+		key[7] = "your";
+		key[8] = "CAT";
+		key[9] = "ACDC";
+		
+		assertEquals(midList.toString(), key.toString());
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	void insertionSort_NullInput()
+	{
+		assertThrows(NullPointerException.class, () -> {d.insertionSort(null, null);});
+	}
+	
+	/**
+	 * 
+	 */
 	@Test
 	void areAnagrams_EmptyString()
 	{
-		
+		assertFalse(AnagramChecker.areAnagrams("cart",""));
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void areAnagrams_arentAnagrams()
+	{
+		assertFalse(AnagramChecker.areAnagrams("cart","meep"));
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void areAnagrams_areAnagrams()
+	{
+		assertTrue(AnagramChecker.areAnagrams("cart","Trac"));
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void areAnagrams_NullInput()
+	{
+		assertThrows(NullPointerException.class, () -> {AnagramChecker.areAnagrams(null, null);});
 	}
 
 }
