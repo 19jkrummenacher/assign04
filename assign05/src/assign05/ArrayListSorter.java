9a461956ff8a5c731d9bb2db69ac8f78f172b95b
jonpokemon43@outlook.com <meepmeep>
2020-09-27 11:48:58 -0600

    assign05

---
+++ assign05/src/assign05/ArrayListSorter.java
@@ -0,0 +1,29 @@
+package assign05;
+
+public class ArrayListSorter
+{
+	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T>)
+	{
+		
+	}
+	
+	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T>)
+	{
+		
+	}
+	
+	public static ArrayList<Integer> generateAscending(int size)
+	{
+		
+	}
+	
+	public static ArrayList<Integer> generatePermuted(int size)
+	{
+		
+	}
+	
+	public static ArrayList<Integer> generateDescending(int size)
+	{
+		
+	}
+}
